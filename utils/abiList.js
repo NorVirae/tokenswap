6bdc7a8dd6393dd2a9c4af40307b57766049514e
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-02-16 16:37:37 +0100

    add abi interface for token for token swap

--- utils/abiList.js
+++ utils/abiList.js
@@ -12,6 +12,7 @@ const pairAbi = [
 
 const routerAbi = [
   "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+  "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
 ];
 
 module.exports = {
